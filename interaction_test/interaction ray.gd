c2d239d7d3e799637d680a77fd8a05fca7c4e7d9
Cpl-flowers <kevingabrielflores@hotmail.com>
2024-07-29 02:37:37 -0500

    Got throwables working
    -throwables are now able to be thrown with the LMB
    
    -next version will add splatter and effect to vials for ingame experience

--- interaction_test/interaction ray.gd
+++ interaction_test/interaction ray.gd
@@ -1,17 +1,16 @@
 extends RayCast3D
 
-@onready var prompt = get_tree().get_root().get_node("InteractLabel")
+@onready var prompt = %InteractLabel
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
-	#prompt.text = ""
+	prompt.text = ""
 	
 	if is_colliding():
 		var collider = get_collider()
-		prompt.text = collider.get_parent().name
 		
 		if collider is Interactable:
 			prompt.text = collider.get_prompt()
 
-			if Input.is_action_just_pressed(collider.prompt_message):
+			if Input.is_action_just_pressed(collider.prompt_input.to_upper()):
 				collider.interact(owner)
