846046a9f58ab0f8de81fd33f269259c5fa6fe97
Don <90371458+DonatoClemente@users.noreply.github.com>
2024-08-05 20:15:48 -0500

    Merge Compatability Check
    Checked in-editor for any breaks from branch merging.

--- interactable items/GreenVileScript.gd
+++ interactable items/GreenVileScript.gd
@@ -4,11 +4,8 @@ var smashed: bool = false
 var particle = preload("res://gpu_particles_3d.tscn")
 
 func _on_body_entered(_body):
-	
 	var particleins = particle.instantiate()
-	
 	if not smashed:
-		
 		particleins.position = global_position
 		particleins.emitting = true
 		get_tree().current_scene.add_child(particleins)
--- interactable items/donsgreenvial.gd
+++ interactable items/donsgreenvial.gd
@@ -8,11 +8,11 @@ var displacement = 5
 func _on_body_entered(_body):
 	if not has_splat:
 		has_splat = true
-
-func _physics_process(_delta):
-	if has_splat:
 		var normal = (position - current_pos).normalized()
 		var new_splat = phys_splat.instantiate()
 		get_tree().get_root().add_child(new_splat)
 		new_splat.global_position = global_position - (normal * displacement)
 		queue_free()
+
+#func _physics_process(_delta):
+	#if has_splat:
